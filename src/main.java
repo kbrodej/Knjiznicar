8f9d5c1d1c530e38d6b62da5624bf878226eae9b
kbrodej <klemen.brodej1@gmail.com>
2016-06-01 22:31:25 +0200

    all three search methods finished

--- src/main.java
+++ src/main.java
@@ -3,11 +3,74 @@
  */
 import java.sql.*;
 public class main {
-    public static void isciPoKnjigi(String bookName){
-
+    public static void isciPoKnjigi(String bookName, Connection conn){
+        try {
+            PreparedStatement stmt = null;
+            String sql = "SELECT b.id_book,b.title, b.ISBN, a.name,a.surname, bs.status,l.language,t.type,t.max_borrowed_days FROM book b\n" +
+                    "  join bookauthor ba USING(id_book) JOIN author a USING(id_author)\n" +
+                    "  join book_status bs using(id_book_status)\n" +
+                    "  join language l using(id_language)\n" +
+                    "  join type t using(id_type)\n" +
+                    "where title = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1,bookName);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String status ="";
+                int id_book = rs.getInt("id_book");
+                String title = rs.getString("title");
+                String aName = rs.getString("name");
+                String aSurname = rs.getString("surname");
+                int id_book_status = rs.getInt("status");
+                String language = rs.getString("language");
+                String type = rs.getString("type");
+                switch (id_book_status) {
+                    case 1: status = "Na voljo";
+                        break;
+                    case 2: status = "Ni na volojo";
+                }
+                int max_bd = rs.getInt("max_borrowed_days");
+                System.out.println(id_book + " " + title + " " + " " + status + " " + aName + " " + aSurname + " " + id_book_status + " "
+                        + language + " " + type + " " + max_bd);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
-    public static void isciPoAvtorju(String name, String surname){
-
+    public static void isciPoAvtorju(String name, String surname, Connection conn){
+        try {
+            PreparedStatement stmt = null;
+            String sql = "SELECT b.id_book,b.title, b.ISBN, a.name,a.surname, bs.status,l.language,t.type,t.max_borrowed_days FROM bookauthor ba\n" +
+                    "JOIN book b USING(id_book) JOIN author a USING(id_author)\n" +
+                    "  JOIN book_status bs USING(id_book_status)\n" +
+                    "  JOIN language l USING(id_language)\n" +
+                    "  JOIN type t USING(id_type)\n" +
+                    "WHERE a.name = ? AND a.surname =?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, surname);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String status ="";
+                int id_book = rs.getInt("id_book");
+                String title = rs.getString("title");
+                String aName = rs.getString("name");
+                String aSurname = rs.getString("surname");
+                int id_book_status = rs.getInt("status");
+                String language = rs.getString("language");
+                String type = rs.getString("type");
+                switch (id_book_status) {
+                    case 1: status = "Na voljo";
+                        break;
+                    case 2: status = "Ni na volojo";
+                }
+                int max_bd = rs.getInt("max_borrowed_days");
+                System.out.println(id_book + " " + title + " " + " " + status + " " + aName + " " + aSurname + " " + id_book_status + " "
+                        + language + " " + type + " " + max_bd);
+            }
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
     public static void isciPoISBN(int ISBN,Connection conn) {
         System.out.println("Iskanje po ISBN");
@@ -71,6 +134,8 @@ public class main {
             if(conn.isValid(10)) {
                 System.out.println("Connecton succesful!");
                 isciPoISBN(123456789,conn);
+                isciPoAvtorju("Klemen","Brodej",conn);
+                isciPoKnjigi("Moja prva knjiga",conn);
 
             } else {
                 throw new SQLException();
